1f437706d844745c6105198be56c90ce57613533
TheWander02 <48934424+thewander02@users.noreply.github.com>
2024-10-18 19:35:11 -0700

    i am dumb

--- lua/refresh.lua
+++ lua/refresh.lua
@@ -8,7 +8,7 @@ LuaCraft.broadcastMessage("--- Refreshing Scripts ---")
 
 for k, v in ipairs(scripts) do
   LuaCraft.broadcastMessage("Downloading ".. v)
-  LuaCraft.writeFile("../../lua/"v".lua", LuaCraft.httpGet("https://raw.githubusercontent.com/thewander02/lua-craft-scripts/refs/heads/main/lua/".. v ..".lua"))
+  LuaCraft.writeFile("../../lua/".. v ..".lua", LuaCraft.httpGet("https://raw.githubusercontent.com/thewander02/lua-craft-scripts/refs/heads/main/lua/".. v ..".lua"))
 end
 
 LuaCraft.broadcastMessage("--------------------------")
