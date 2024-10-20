13385f1f8ba69e108911e6df1d5f3573ed5c5bd9
dankfmemes <dankfmemes@gmail.com>
2024-10-20 12:08:42 -0700

    send url feedback

--- lua/refresh.lua
+++ lua/refresh.lua
@@ -6,14 +6,17 @@ local scriptNames = { 'army.lua', 'battle.lua', 'chunk.lua', 'rtp.lua' }
 LuaCraft.broadcastMessage('> refreshing scripts...')
 
 for _, scriptName in pairs(scriptNames) do
-	local sourceCode = LuaCraft.httpGet('https://raw.githubusercontent.com/' .. repo .. '/refs/heads/' .. branch .. '/lua/' .. scriptName .. '.lua')
+	local expectedURL = 'https://raw.githubusercontent.com/' .. repo .. '/refs/heads/' .. branch .. '/lua/' .. scriptName .. '.lua'
+	local sourceCode = LuaCraft.httpGet(expectedURL)
 	if type(sourceCode) == 'string' then
 		local successfulWrite = LuaCraft.writeFile('../../lua/' .. scriptName, sourceCode)
 		if successfulWrite then
 			LuaCraft.broadcastMessage('> [' .. scriptName .. '] the script was written successfully.')
 		else
-			LuaCraft.broadcastMessage('> [' .. scriptName .. '] ' .. LuaCraft.convertAmpersandToSection('&4failed to write the script!'))
+			LuaCraft.broadcastMessage('> [' .. scriptName .. '] ' .. LuaCraft.convertAmpersandToSection('&cfailed to write the script!'))
 		end
+	else
+		LuaCraft.broadcastMessage('> [' .. scriptName .. '] ' .. LuaCraft.convertAmpersandToSection('&cfailed to load URL: ') .. expectedURL)
 	end
 end
 
