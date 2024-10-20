3d5318a5e47930e01f285184a47cb03b0f39badc
dankfmemes <dankfmemes@gmail.com>
2024-10-20 12:03:27 -0700

    more feedback for refresh

--- lua/refresh.lua
+++ lua/refresh.lua
@@ -8,7 +8,12 @@ LuaCraft.broadcastMessage('> refreshing scripts...')
 for _, scriptName in pairs(scriptNames) do
 	local sourceCode = LuaCraft.httpGet('https://raw.githubusercontent.com/' .. repo .. '/refs/heads/' .. branch .. '/lua/' .. scriptName .. '.lua')
 	if type(sourceCode) == 'string' then
-		LuaCraft.writeFile('../../lua/' .. scriptName, sourceCode)
+		local successfulWrite = LuaCraft.writeFile('../../lua/' .. scriptName, sourceCode)
+		if successfulWrite then
+			LuaCraft.broadcastMessage('> [' .. scriptName .. '] the script was written successfully.')
+		else
+			LuaCraft.broadcastMessage('> [' .. scriptName .. '] ' .. LuaCraft.convertAmpersandToSection('&4failed to write the script!'))
+		end
 	end
 end
 
